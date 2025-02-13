b0de4e72fd93c421e9cf7717219db618774b4146
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2025-02-13 21:29:52 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar.tsx";
 import Hero from "./components/Hero.tsx";
 import Activity from "./components/Activity.tsx";
-import About from "./components/About.tsx";
+// import About from "./components/About.tsx";
 function App() {
   return (
     <section className=" h-screen w-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth overflow-hidden">
